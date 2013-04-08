d3dba6c1905f0bceec5c6dfe95f48d448a183015
Jason Wirth <wirth.jason@gmail.com>
2013-04-08 06:44:04 -0500

    on chapter 5.... just about to start ORM

---
+++ lists/tests.py
@@ -0,0 +1,35 @@
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
+from django.template.loader import render_to_string
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content, expected_html)
+
+
+    def test_home_pagecan_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+
+
+        self.assertIn("A new list item", response.content)
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'A new list item'}
+        )
+        self.assertEqual(response.content, expected_html)
+
+
