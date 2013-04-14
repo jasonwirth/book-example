99b71026289ffc1e50295caefa9c2e80bc97b38e
jasonwirth <wirth.jason@gmail.com>
2013-04-14 11:49:34 -0500

    Refactored functional_tests.py to use .assertIn instead of .assertTrue

--- functional_tests.py
+++ functional_tests.py
@@ -54,18 +54,14 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '2: Use peacock feathers to make a fly' for row in rows),
-            "New to-do item did not appear in table -- its text was:\n%s" % (
-                table.text,
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows]
             )
-        )
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table -- its text was:\n%s" % (
-                table.text,
-            )
-        )
+
+        self.assertIn(
+            '1: Buy peacock feathers', 
+            [row.text for row in rows] )
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generate a unique URL for her -- there is some
