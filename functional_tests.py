83a5af3fca664d88a75666c1b9c6321068679808
jasonwirth <wirth.jason@gmail.com>
2013-04-14 13:11:16 -0500

    Tests finally working.
    Currently it fails on "Finish the test"! Yeah.

--- functional_tests.py
+++ functional_tests.py
@@ -36,13 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         import time
-        time.sleep(10)
+        #time.sleep(10)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', (row.text for row in rows),
-            msg="New to-do item did not appear in table -- its text was:\n%s" % (table.text,)
-        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -51,23 +46,22 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2: Use peacock feathers to make a fly', 
-            [row.text for row in rows]
-            )
+        #time.sleep(10)
 
-        self.assertIn(
-            '1: Buy peacock feathers', 
-            [row.text for row in rows] )
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generate a unique URL for her -- there is some
         # explanatory text to that effect.
         self.fail('Finish the test!')
 
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main()
